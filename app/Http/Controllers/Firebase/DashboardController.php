cbb3599de483f95f862efbc37b00b90c67e85f3c
ronmhel <salcedo.131437@marikina.sti.edu.ph>
2024-08-04 14:30:06 +0800

    Compiled dist files adminlte | blade templating

---
+++ app/Http/Controllers/Firebase/DashboardController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Firebase;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    public function __invoke(Request $request){
+        return view('firebase.admin.dashboard');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\Firebase\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('admin/dashboard', DashboardController::class);
